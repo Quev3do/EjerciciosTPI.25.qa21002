2546ddec5a5faebf279b885071b92744d3c3bfaa
Adan Quevedo <adanque12dst@gmail.com>
2025-09-19 18:58:40 -0600

    ejercicio 1: primas triangular (4, 7)

--- ejercicio1/index.php
+++ ejercicio1/index.php
@@ -1,4 +1,17 @@
 <?php
 //recuerda usar el localhost:60940, ya que lo mapeaste a ese puerto!
+function calculo($base, $altura){
+    $a = ((1/2)*$base)**2;
+    $c = $base ** 2;
+    $b = sqrt($c - $a);
 
+    $areaT = (1/2)*$base*$b;
+
+    $volumen = $areaT * $altura;
+    return ($volumen);
+}
+
+$vol = round(calculo(4, 7), 2);
+
+echo "El volumen es: $vol unidades cubicas.";
 ?>
