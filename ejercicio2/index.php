3d0c95e4fecfddf5537affa852567298ecafcb55
Adan Quevedo <adanque12dst@gmail.com>
2025-09-20 06:48:55 -0600

    base ejercicio 2

---
+++ ejercicio2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cono truncado</title>
+</head>
+<body>
+    <form action="" method>
+        <label for="rbase">Radio base: </label>
+        <input type="text" name="rbase" id="rbase"><br>
+        <label for="rtop">Radio arriba: </label>
+        <input type="text" name="rtop" id="rtop"><br>
+        <label for="altura">Altura cono: </label>
+        <input type="text" name="altura" id="altura"><br>
+        <button type="submit">Calcular volumen</button>
+    </form>
+    <?php
+    
+    ?>
+</body>
+</html>
