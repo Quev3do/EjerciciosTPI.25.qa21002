5357ef9a4c433bb304a6dcfa0aa8cdf2ec340a3d
Adan Quevedo <adanque12dst@gmail.com>
2025-09-23 10:20:26 -0600

    base ejercicio 4

---
+++ ejercicio4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Volumen piscina</title>
+</head>
+<body>
+    <form action="" method="post">
+        Volumen: <input type="number" name="volumen" id="volumen" step="0.1">
+        Radio b: <input type="number" name="radio" id="radio" step="0.1">
+        <button type="submit">Calcular altura!</button>
+    </form>
+    
+</body>
+</html>
