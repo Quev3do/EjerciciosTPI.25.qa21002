14ee2b59c113e1845d23f440a8f3c639e071a5b8
Adan Quevedo <adanque12dst@gmail.com>
2025-09-23 11:30:59 -0600

    base ejercicio 7

---
+++ ejercicio7/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AL pendiente de ti</title>
+</head>
+<body>
+    <form action="" method="post">
+
+    </form>
+    <?php
+    ?>
+</body>
+</html>
