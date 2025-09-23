b9e4d1e3273558037383ba0895904acb519ef573
Adan Quevedo <adanque12dst@gmail.com>
2025-09-23 10:45:16 -0600

    base ejercicio 5

---
+++ ejercicio5/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Don Martin</title>
+</head>
+<body>
+    <?php
+    $inversion = 93504;
+    ?>
+</body>
+</html>
