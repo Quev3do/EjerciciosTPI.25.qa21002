744bbd0a31cd6680f2fe9dbdc2217baa2f1231c2
Adan Quevedo <adanque12dst@gmail.com>
2025-09-23 10:52:04 -0600

    ejercicio 5: Don Marti y su LAVADO de autos chanchona 503

--- ejercicio5/index.php
+++ ejercicio5/index.php
@@ -8,6 +8,20 @@
 <body>
     <?php
     $inversion = 93504;
+    $ganancia = 0.323;
+
+    $anual = $inversion * $ganancia;
+    $meses = $anual / 12;
+    $semanal = $meses / 4;
+
+    $primero = $meses * 7;
+    $segundo = $anual*8 + $meses*2 + $semanal;
+ 
+    ?>
+    <h1>DON MARTIN GANANAICAS</h1>
+    <h4>7 meses: <?=$primero?></h4>
+    <h4>8 años, 2 meses, 7 dias: <?=$segundo?></h4>
+    <?php
     ?>
 </body>
 </html>
